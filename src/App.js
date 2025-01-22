fb0c768f6de5a9adb121e2cbba16169f96522f2e
Aman8593 <amanwadgaonkar8593@gmail.com>
2025-01-22 23:13:15 +0530

    navbar-done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
-import { EnvironmentOutlined } from "@ant-design/icons";
+import { EnvironmentOutlined, PhoneOutlined } from "@ant-design/icons";
 import "./App.scss";
+import { Button } from "antd";
 
 function App() {
   return (
@@ -21,13 +22,33 @@ function App() {
                 <EnvironmentOutlined height="50" />
               </div>
               <div className="data">
-                <p>Location</p>
-                <p>Nagpur</p>
+                <p className="data-content-1">Location</p>
+                <p className="data-content-2">Nagpur</p>
               </div>
             </div>
-            <div className="navbar-contact">Cont</div>
-            <div className="navbar-join-btn">Btn</div>
+            <div className="navbar-location">
+              <div className="icon">
+                <PhoneOutlined height="50" />
+              </div>
+              <div className="data">
+                <p className="data-content-1">Contact</p>
+                <p className="data-content-2">+91-9370980349</p>
+              </div>
+            </div>
+            <div className="navbar-join-btn">
+              <Button type="primary" >Schedule Demo</Button>
+            </div>
           </div>
+        </div>
+        <div className="main-pg-navlinks">
+          <li><a href=""> About Us</a></li>
+          <li><a href=""> Courses</a></li>
+          <li><a href=""> Services</a></li>
+          <li><a href=""> Blogs</a></li>
+          <li><a href=""> Contact Us</a></li>
+        </div>
+        <div className="main-pg-homepg">
+          
         </div>
       </div>
     </div>
