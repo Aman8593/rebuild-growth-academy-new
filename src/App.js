5723e4a1d368d07db611d081196de8866a750030
Eamin05 <eaminjawed@gmail.com>
2025-01-27 01:32:11 +0530

    react slider sample code - not working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 // import logo from './logo.svg';
 import { EnvironmentOutlined, PhoneOutlined } from "@ant-design/icons";
-import "./App.scss";
 import { Button } from "antd";
+import Slider from "react-slick";
+import "./App.scss";
 
 function App() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div className="main-page">
       <div className="main-pg-container">
@@ -290,6 +298,27 @@ function App() {
           </div>
         </div>
       </div>
+
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
     </div>
   );
 }
