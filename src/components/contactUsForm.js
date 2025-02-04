17b2b71810caf0dc09e165b281b1fc2664ca66e5
Aman8593 <amanwadgaonkar8593@gmail.com>
2025-02-04 21:24:29 +0530

    emailjs-done

--- src/components/contactUsForm.js
+++ src/components/contactUsForm.js
@@ -2,6 +2,8 @@ import { Button, Form, Input } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import React, { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
+import axios from "axios";
+
 
 const ContactUsFormComp = () => {
   const [formData, setFormData] = useState({
@@ -32,22 +34,27 @@ const ContactUsFormComp = () => {
   const handleSubmit = async (event) => {
     // event.preventDefault();
     setLoading(true);
-
+    const emailJsUrl = "https://api.emailjs.com/api/v1.0/email/send";
+    const emailData = {
+      service_id: "service_8eyv5z8",
+      template_id: "template_ntu90ag",
+      user_id: "41LseswCEaTaksK_z",
+      template_params: {
+        to_name: "Aman",
+        user_name: formData.name,
+        user_email: formData.email,
+        phone_no: formData.phone_no,
+        message: formData.message,
+      },
+    };
     const controller = new AbortController();
     const timeoutId = setTimeout(() => controller.abort(), 5000);
 
     try {
-      // const response = await fetch("https://formsubmit.co/ajax/test@test.com", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify(formData),
-      //   signal: controller.signal,
-      // });
-
+      const response = await axios.post(emailJsUrl, emailData);
+      console.log(response.data);
       // change to response.ok
-      if (true) {
+      if (response.status === 200) {
         notifySuccess();
         setFormData({
           name: "",
@@ -72,18 +79,34 @@ const ContactUsFormComp = () => {
       <h3>Contact US</h3>
       <Form onFinish={handleSubmit}>
         <Form.Item label="Full Name" name="name" required>
-          <Input placeholder="Enter your name." type="text" onChange={handleChange} />
+          <Input
+            placeholder="Enter your name."
+            value={formData.name}
+            type="text"
+            onChange={handleChange}
+          />
         </Form.Item>
         <Form.Item label="Email address" name="email" required>
-          <Input placeholder="Enter your email." type="email" onChange={handleChange} />
+          <Input
+            placeholder="Enter your email."
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
         </Form.Item>
         <Form.Item label="Phone Number" name="phone_no" required>
-          <Input placeholder="Enter your phone number." type="number" onChange={handleChange} />
+          <Input
+            placeholder="Enter your phone number."
+            type="number"
+            value={formData.phone_no}
+            onChange={handleChange}
+          />
         </Form.Item>
         <Form.Item label="Message" name="message" type="text">
           <TextArea
             placeholder="Maximum 200 characters allowed"
             maxLength={200}
+            value={formData.message}
             onChange={handleChange}
           />
         </Form.Item>
